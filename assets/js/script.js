27ea9303d22dd6812e11e2aba4a3c5acdf131ed5
Oliver Mandila <olivermandila@gmail.com>
2024-04-12 07:02:23 +0200

    feat: add process options function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,28 @@
-const API_KEY = '';
+const API_KEY = 'rwzTKW5fthM-rPBOe1AoNvpwEUs';
 const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById('resultsModal'));
 
 document.getElementById('status').addEventListener('click', e => getStatus(e));
 document.getElementById('submit').addEventListener('click', e => postForm(e));
 
+function processOptions(form){
+    
+    let optArray = [];
+
+    for (let entry of form.entries()) {
+        if (entry[0] === 'options') {
+            optArray.push(entry[1]);
+        }
+    }
+    form.delete('option');
+
+    form.append('options', optArray.join());
+
+    return form;
+}
+
 async function postForm(e){
-    const form = new FormData(document.getElementById('checksform'));
+    const form = processOptions(new FormData(document.getElementById('checksform')));
 
     const response = await fetch(API_URL, {
                         method: "POST",
